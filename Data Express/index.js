8fa3b72ee18e6f5540b0961e981b93ff20a1302b
Jacob Valentine <JValentine@student.neumont.edu>
2017-05-31 09:50:34 -0600

    Extraction
    Took Apart the Questionable Zip

---
+++ Data Express/index.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+    pug = require('pug')
+    path = require('path')
+    var app = express();
+
+    app.set('view engine', 'pug');
+    app.set('views',__dirname + '/views');
+    app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/',function(req, res){
+    res.render("Index");
+});
+
+    app.listen(3000)
