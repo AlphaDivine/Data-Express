9e60b0e23dd59a966b60f166fd2b9688ff829b48
Jacob Valentine <JValentine@student.neumont.edu>
2017-06-02 10:13:37 -0600

    Added some stuff

--- Data Express/index.js
+++ Data Express/index.js
@@ -2,12 +2,17 @@ var express = require('express'),
     pug = require('pug'),
     path = require('path'),
     route = require('./routes/routes.js');
+    bodyParser = require('body-parser');
     
 var app = express();
 
 app.set('view engine', 'pug');
 app.set('views',__dirname + '/views');
 app.use(express.static(path.join(__dirname + '/public')));
+
+var urlencodedParser = bodyParser.urlencoded({
+  extended: true
+})
 
 app.get('/', route.index);
 app.get('/create', route.create);
