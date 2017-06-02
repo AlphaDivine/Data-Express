780082237f177ffac6ab8a83f1229d9f9b212e3e
Jacob Valentine <JValentine@student.neumont.edu>
2017-06-02 09:50:09 -0600

    Edited Index
    Added routes for the database manipulation

--- Data Express/index.js
+++ Data Express/index.js
@@ -1,11 +1,21 @@
 var express = require('express'),
-    pug = require('pug')
-    path = require('path')
-    var app = express();
+    pug = require('pug'),
+    path = require('path'),
+    route = require('./routes/routes.js');
+    
+var app = express();
 
-    app.set('view engine', 'pug');
-    app.set('views',__dirname + '/views');
-    app.use(express.static(path.join(__dirname + '/public')));
+app.set('view engine', 'pug');
+app.set('views',__dirname + '/views');
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.get('/', route.index);
+app.get('/create', route.create);
+app.get('/edit/:id', route.edit);
+app.get('/details/:id', route.details);
+app.post('/create', urlencodedParser, route.createPerson);
+app.post('/edit/:id', urlencodedParser, route.editPerson);
+app.get('/delete/:id', route.delete);
 
 app.get('/',function(req, res){
     res.render("Index");
